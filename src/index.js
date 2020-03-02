467a2b2967633f3b00965a42fef3b830ff6ad2db
sequ3l <61327461+sequ3l@users.noreply.github.com>
2020-03-02 22:35:13 +0200

    noModelSuffix, fileNameMatchesModel default false

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ class Automate {
     const defaultOptions = {
       type: 'js', // Which code style want to generate, supported: js/ts/egg/midway. Default is `js`.
       camelCase: false, // Model name camel case. Default is false.
-      noModelSuffix: true,
+      noModelSuffix: false,
       fileNameCamelCase: false, // Model file name camel case. Default is false.
-      fileNameMatchesModel: true,
+      fileNameMatchesModel: false,
       dir: 'models', // What directory to place the models. Default is `models`.
       typesDir: null, // What directory to place the models' definitions (for typescript), default is the same with dir.
       emptyDir: false, // Remove all files in `dir` and `typesDir` directories before generate models.
